21f37c1f0e9b66a6a1298065e3d47e8156fb5d47
Machiavelli Fernandes <machiavellif@ps.studio>
2019-08-07 15:31:24 +0200

    first commit

---
+++ github.js
@@ -0,0 +1,21 @@
+class GitHub {
+    constructor() {
+        this.client_id = '801cd9e402ad4fe69926';
+        this.client_secret = '80209d5b41119e5464ed73245306b3a8d24b2d11';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc'
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+        return {
+            profile,
+            repos
+        }
+    }
+}
